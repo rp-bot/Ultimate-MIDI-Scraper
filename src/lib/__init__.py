20b668ec4884508f9894a54d5554605992798120
rp-bot_ubuntu <legacy.business18@gmail.com>
2023-06-01 23:51:54 -0400

    fixed file names

--- src/lib/__init__.py
+++ src/lib/__init__.py
@@ -2,6 +2,7 @@
 # init file for folder level 2,
 import os
 import sys
+from datetime import datetime
 currentdir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 parentdir = os.path.dirname(currentdir)
 sys.path.append(parentdir)
@@ -12,3 +13,8 @@ STR_TABLE = str.maketrans({"-": "_", " ": "_"})
 # Path to MIDI data directory
 path_to_MIDIdata = ['data', 'MIDIdata']
 MIDI_DATA_DIR = os.path.join(os.path.abspath(''), *path_to_MIDIdata)
+
+path_to_releases = ['data', 'releases']
+RELEASES_DIR = os.path.join(os.path.abspath(''), *path_to_releases)
+
+TODAY = datetime.now().strftime("%m-%d-%Y")
--- src/lib/release_ops.py
+++ src/lib/release_ops.py
@@ -1,6 +1,9 @@
 from __init__ import *  # MIDI_DATA_DIR, RELEASES_DIR,TODAY
 import os
+import string
+import re
 import shutil
+from tqdm import tqdm
 
 
 def zipper(directory_path: str, destination_path: str, file_name: str, compression_format="zip"):
@@ -17,12 +20,29 @@ def zipper(directory_path: str, destination_path: str, file_name: str, compressi
         # Move the zipped file to the current working directory
         shutil.move(f"{file_name}.zip", destination_path)
 
-        print(f"Directory successfully zipped: {file_name}.{compression_format}\n")
+        print(
+            f"Directory successfully zipped: {file_name}.{compression_format}\n")
 
     except Exception as e:
         print(f"An error occurred: {str(e)}")
 
 
-if __name__ == '__main__':
+def sanitize_filename(filename):
+    valid_chars = "-_.() %s%s" % (string.ascii_letters, string.digits)
+    sanitized_filename = ''.join(c for c in filename if c in valid_chars)
+    sanitized_filename = sanitized_filename.replace(' ', '_')
+    return sanitized_filename
 
+
+def rename_files_in_directory(directory_path):
+    for filename in tqdm(os.listdir(directory_path)):
+        if os.path.isfile(os.path.join(directory_path, filename)):
+            new_filename = sanitize_filename(filename)
+            if new_filename != filename:
+                os.rename(os.path.join(directory_path, filename),
+                          os.path.join(directory_path, new_filename))
+
+
+if __name__ == '__main__':
+    # rename_files_in_directory(os.path.join(MIDI_DATA_DIR, "country"))
     zipper(MIDI_DATA_DIR, RELEASES_DIR, f"{TODAY}_midi_files")
